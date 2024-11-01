168bb7e238855005b971da49f0fbff7c85aa4d39
anggiirawan13 <irawananggi76@gmail.com>
2024-11-01 22:22:27 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\ResiController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(UserController::class)->prefix('/user')->group(function () {
-    Route::get('/', 'index');
-});
+// Route::controller(UserController::class)->prefix('/user')->group(function () {
+//     Route::get('/', 'index');
+// });
 
 Route::get('/', function () {
     return view('welcome');
